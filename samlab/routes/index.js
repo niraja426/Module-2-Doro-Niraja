34c6032f5a12c0b4677e2c3b8a83400ec2683156
Doro Hassan <dorothea.hassan@gmail.com>
2019-09-09 10:46:02 +0200

    create login and contact us file and the routes for login and contact us in the index.js folder

--- samlab/routes/index.js
+++ samlab/routes/index.js
@@ -10,6 +10,15 @@ router.get("/about",(req,res)=>{
   res.render("about");
 });
 
+router.get("/contact_us",(req,res)=>{
+  res.render("contact_us");
+});
+
+router.get("/login",(req,res)=>{
+  res.render("login");
+});
+
+
 
 
 module.exports = router;
