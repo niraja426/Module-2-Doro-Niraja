f2ed4f2bc1202353fb1f96d3e641f514ffc8eae7
Doro Hassan <dorothea.hassan@gmail.com>
2019-09-09 13:30:49 +0200

    Doro: changes done at nav.hbs: contact link changed ; index.js changed: added new route to the message_sent.hbs file, changed the route to the contact file ; added new files: message_sent.hbs and contact.js ; get route done still working on the post route

--- samlab/routes/index.js
+++ samlab/routes/index.js
@@ -6,18 +6,21 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get("/about",(req, res, next)=>{
+router.get("/about",(req, res, next) =>{
   res.render("about");
 });
 
-router.get("/contact_us",(req, res, next)=>{
+router.get("/contact",(req, res, next) =>{
   res.render("contact_us");
 });
 
-router.get("/login",(req, res, next)=>{
+router.get("/login",(req, res, next) =>{
   res.render("login");
 });
 
+router.get("/contact/message_sent", (req, res, next) => {
+  res.render("message_sent");
+})
 
 
 
