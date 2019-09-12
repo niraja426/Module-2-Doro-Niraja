b9d1258ae1a179a2f1eda3e3ff47df4662b01212
niraja426 <nrjshk@gmail.com>
2019-09-12 15:28:17 +0200

    mongo

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app          = express();
 ////////////////////////////////////////////
 // require("./config/mongodb"); // database initial setup
 mongoose
-  .connect('mongodb://localhost/samlab', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
